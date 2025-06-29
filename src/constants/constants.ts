ceab17d5e182c301d61a1effd39d9207ea2ba75e
lecto17 <khwoo_rpsit@naver.com>
2025-06-29 09:57:03 +0900

    chore: gnb, bnb 메뉴 상수화

---
+++ src/constants/constants.ts
@@ -0,0 +1,45 @@
+export const GlobalMenus = {
+  "/": {
+    title: "",
+    firstIcon: "search",
+    secondtIcon: "heart",
+  },
+  "/challenge": {
+    title: "챌린지",
+    firstIcon: "search",
+    secondtIcon: "heart",
+  },
+  "/certification": {
+    title: "챌린지 인증",
+    firstIcon: "search",
+    secondtIcon: "heart",
+  },
+  "/mypage": {
+    title: "마이페이지",
+    firstIcon: "bell",
+    secondtIcon: "setting",
+  },
+};
+
+export const BottomMenus = [
+  {
+    url: "/",
+    title: "홈",
+    icon: "",
+  },
+  {
+    url: "/challenge",
+    title: "챌린지",
+    icon: "",
+  },
+  {
+    url: "/certification",
+    title: "인증",
+    icon: "",
+  },
+  {
+    url: "/mypage",
+    title: "마이페이지",
+    icon: "",
+  },
+];
