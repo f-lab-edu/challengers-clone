3ab2e4f0e49c4b027af79769adf95aba1a6548ac
lecto17 <khwoo_rpsit@naver.com>
2025-06-30 15:53:48 +0900

    chore: HomeActionIcons 컴포넌트 작업

---
+++ src/components/home/HomeActionIcons.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import ScrollableArea from "@/components/ScrollableArea";
+import { HOME_ACTION_ICONS } from "@/type/home";
+import Image from "next/image";
+import styled from "styled-components";
+
+type HomeActionIconsProps = {
+  items: HOME_ACTION_ICONS[];
+};
+
+export default function HomeActionIcons({ items }: HomeActionIconsProps) {
+  return (
+    <Container>
+      <ScrollableArea>
+        {items.map((item) => (
+          <Item key={item.name}>
+            <Image
+              src={item.iconSrc}
+              width={48}
+              height={48}
+              alt={`${item.name} icon`}
+            />
+            <ItemName>{item.name}</ItemName>
+          </Item>
+        ))}
+      </ScrollableArea>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  margin-bottom: 20px;
+`;
+
+const Item = styled.li`
+  display: flex;
+  min-width: 88px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 8px 0 0;
+`;
+
+const ItemName = styled.span`
+  font-size: 14px;
+`;
