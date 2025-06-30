3f95b32f0624643e9d582b303befb621425a959d
lecto17 <khwoo_rpsit@naver.com>
2025-06-30 14:03:20 +0900

    feat: 홈 배너 carousel 컴포넌트 추가

---
+++ src/components/home/HomeBannerCarousel.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+// ISR
+import { HOME_CAROUSEL_ITEM } from "@/type/home";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import styled from "styled-components";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
+type HomeBannerCarouselProps = {
+  data: HOME_CAROUSEL_ITEM[];
+};
+
+export default function HomeBannerCarousel({ data }: HomeBannerCarouselProps) {
+  const route = useRouter();
+  const handleClickCta = (itemId: string) => {
+    route.push(`/item/${itemId}`);
+  };
+
+  return (
+    <SwiperWrapper
+      modules={[Autoplay]}
+      spaceBetween={16}
+      slidesPerView={1}
+      loop={true}
+      autoplay={{ delay: 4000 }}
+    >
+      {data.map((el) => (
+        <SwiperSlideContainer key={el.itemId}>
+          <SlideTextWrapper>
+            <SubTitle>{el.subtitle}</SubTitle>
+            <Title>{el.title}</Title>
+            <Cta onClick={() => handleClickCta(el.itemId)}>{el.ctaText}</Cta>
+          </SlideTextWrapper>
+          <SlideImageWrapper>
+            <SlideImage
+              src={el.imageSrc}
+              width={100}
+              height={100}
+              alt={el.imageAlt}
+            />
+          </SlideImageWrapper>
+        </SwiperSlideContainer>
+      ))}
+    </SwiperWrapper>
+  );
+}
+
+const SwiperWrapper = styled(Swiper)`
+  width: 100%;
+  height: fit-content;
+  display: flex;
+  position: relative;
+`;
+
+const SwiperSlideContainer = styled(SwiperSlide)`
+  display: flex;
+`;
+
+const SlideTextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 12px;
+  flex-basis: 70%;
+`;
+
+const SlideImageWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  flex-basis: 30%;
+  z-index: -1;
+  height: 100%;
+`;
+
+const SlideImage = styled(Image)`
+  display: flex;
+  width: 100%;
+  height: 100%;
+`;
+
+const SubTitle = styled.h3`
+  font-weight: 400;
+`;
+
+const Title = styled.h2`
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 1.5;
+  white-space: pre-line;
+`;
+
+const Cta = styled.span``;
