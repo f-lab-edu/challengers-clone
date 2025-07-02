26c1a354e513aa666601535d7f9e7aacb44c6873
lecto17 <khwoo_rpsit@naver.com>
2025-07-02 21:03:44 +0900

    chore: 카테고리 아이템 타입 및 mock data 추가

--- src/type/home.ts
+++ src/type/home.ts
@@ -13,3 +13,20 @@ export type HOME_ACTION_ICON = {
   name: string;
   href: string;
 };
+
+export type HOME_CATEGORY_ITEM = {
+  itemId: string;
+  imageSrc: string; // 상품 이미지
+  imageAlt: string; // 상품 이미지 설명
+  isLiked: boolean; // 찜하기 여부
+  currentCount: number; // 현재 참여 인원
+  maxCount: number; // 최대 인원 수
+  brand: string; // 브랜드명
+  brandId: "musinsa-id" | "olive-young-id" | "11st-id" | "g-market-id"; // 브랜드 id
+  startTime: string; // 시작 시간 (ex. "6/27 오후 1시")
+  endTime: string; // 종료 시간 (ex. "4시")
+  title: string; // 상품명
+  price: number; // 현재 가격
+  originalPrice: number; // 원래 가격
+  cashbackAmount: number; // 페이백 금액
+};
