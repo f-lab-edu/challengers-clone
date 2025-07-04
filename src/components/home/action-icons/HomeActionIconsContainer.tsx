03db0add1eb938ae4cdd76cdc881b86831d8cf1a
lecto17 <khwoo_rpsit@naver.com>
2025-07-04 16:38:05 +0900

    chore: container 컴포넌트 rcc로 변경하여 data fetching

--- src/components/home/action-icons/HomeActionIconsContainer.tsx
+++ src/components/home/action-icons/HomeActionIconsContainer.tsx
@@ -1,10 +1,27 @@
+"use client";
+
 import HomeActionIcons from "@/components/home/action-icons/HomeActionIcons";
-import { HOME_ACTION_ICONS } from "@/data/data";
 import { fetchHomeActionIcons } from "@/remotes/home";
+import { HOME_ACTION_ICON } from "@/type/home";
+import { useState } from "react";
+import { useEffect } from "react";
 
-export default async function HomeActionIconsContainer() {
-  const data = HOME_ACTION_ICONS;
-  // const data = await fetchHomeActionIcons();
+export default function HomeActionIconsContainer() {
+  /**
+   * TODO
+   * RSC 환경에서 Api Call 할 것
+   */
+  // const data = await fetchHomeActionIcons()
 
+  const [data, setData] = useState<HOME_ACTION_ICON[]>([]);
+
+  const initData = async () => {
+    const res = await fetchHomeActionIcons();
+    setData(res.data || []);
+  };
+
+  useEffect(() => {
+    initData();
+  }, []);
   return <HomeActionIcons items={data} />;
 }
