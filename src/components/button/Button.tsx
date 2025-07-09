b9a08cab5b8a5e268d63b3c25e26ea2dc8dc2a23
lecto17 <khwoo_rpsit@naver.com>
2025-07-09 15:08:49 +0900

    feat: 공용으로 사용할 버튼 컴포넌트 생성

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,45 @@
+import { ButtonStyle, ButtonType } from "@/constants/button";
+import styled from "styled-components";
+
+type ButtonTypeVariant = keyof typeof ButtonType;
+type ButtonStyleVariant = keyof typeof ButtonStyle;
+
+type ButtonProps = {
+  buttonText: string;
+  onClick: () => void;
+  buttonType: ButtonTypeVariant;
+  buttonStyle: ButtonStyleVariant;
+};
+
+export default function Button({
+  buttonText,
+  onClick,
+  buttonType,
+  buttonStyle,
+}: ButtonProps) {
+  return (
+    <ButtonWrapper
+      onClick={onClick}
+      $buttonType={buttonType}
+      $buttonStyle={buttonStyle}
+    >
+      {buttonText}
+    </ButtonWrapper>
+  );
+}
+
+const ButtonWrapper = styled.button<{
+  $buttonType: ButtonTypeVariant;
+  $buttonStyle: ButtonStyleVariant;
+}>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 12px 0;
+  font-size: 16px;
+  font-weight: 700;
+
+  ${({ $buttonType }) => ButtonType[$buttonType]}
+  ${({ $buttonStyle }) => ButtonStyle[$buttonStyle]}
+`;
