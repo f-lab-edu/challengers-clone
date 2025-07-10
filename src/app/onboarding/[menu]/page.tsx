a4f6ea546551adfad85ed3d2b363624106b95c3b
lecto17 <khwoo_rpsit@naver.com>
2025-07-10 15:57:41 +0900

    chore: onboarding 메뉴 query string이 아닌 path로 받도록 변경

--- src/app/onboarding/[menu]/page.tsx
+++ src/app/onboarding/[menu]/page.tsx
@@ -4,20 +4,25 @@ import styles from "./page.module.css";
 import { redirect } from "next/navigation";
 
 type PageProps = {
-  searchParams: { menu: string } | Promise<{ menu: string }>;
+  params: { menu: string } | Promise<{ menu: string }>;
 };
 
-export default async function page({ searchParams }: PageProps) {
-  const params = await searchParams;
-  const menu = params["menu"] ?? "cosmetics";
+export default async function page({ params }: PageProps) {
+  const { menu } = await params;
 
-  return menu === "cosmetics" ? (
+  if (menu !== "cosmetics") {
+    redirect("/home");
+  }
+
+  return (
     <div className={styles.layout}>
       <PageTransition animationType="load">
         <Onboarding />
       </PageTransition>
     </div>
-  ) : (
-    redirect("/home")
   );
 }
+
+export const generateStaticParams = async () => {
+  return [{ menu: "cosmetics" }];
+};
