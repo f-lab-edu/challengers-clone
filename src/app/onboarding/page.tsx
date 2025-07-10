62fbefaddab0d7cc61841aa9ad702624b43b218b
lecto17 <khwoo_rpsit@naver.com>
2025-07-10 15:10:06 +0900

    chore: onboarding 메뉴에 따라 컴포넌트 지정

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,13 +1,23 @@
 import PageTransition from "@/components/page-transition/PageTransition";
 import Onboarding from "@/components/onboarding/Onboarding";
 import styles from "./page.module.css";
+import { redirect } from "next/navigation";
 
-export default function page() {
-  return (
+type PageProps = {
+  searchParams: { menu: string } | Promise<{ menu: string }>;
+};
+
+export default async function page({ searchParams }: PageProps) {
+  const params = await searchParams;
+  const menu = params["menu"] ?? "cosmetics";
+
+  return menu === "cosmetics" ? (
     <div className={styles.layout}>
       <PageTransition animationType="load">
         <Onboarding />
       </PageTransition>
     </div>
+  ) : (
+    redirect("/home")
   );
 }
