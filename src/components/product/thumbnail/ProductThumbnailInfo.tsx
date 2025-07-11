f3ff4a105a4ba53531c651e5c151ce6eaa54c546
lecto17 <khwoo_rpsit@naver.com>
2025-07-11 12:45:28 +0900

    chore: 썸네일 상품 정보 표시 컴포넌트 생성

---
+++ src/components/product/thumbnail/ProductThumbnailInfo.tsx
@@ -0,0 +1,38 @@
+import Brand, { BRAND_COLOR } from "@/components/chip/Brand";
+import React from "react";
+import styled from "styled-components";
+
+const ProductThumbnailInfo = ({
+  brand,
+  brandId,
+  title,
+  children,
+}: {
+  brand: string;
+  brandId: keyof typeof BRAND_COLOR;
+  title: string;
+  children?: React.ReactNode;
+}) => {
+  return (
+    <>
+      <Brand brandName={brand} brandId={brandId} />
+      {children}
+      <Title>{title}</Title>
+    </>
+  );
+};
+
+const Title = styled.div`
+  margin-top: 4px;
+  font-size: 14px;
+  font-weight: bold;
+  color: #222;
+  line-height: 1.4;
+  display: -webkit-box;
+  -webkit-line-clamp: 2; /* 최대 2줄 */
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  height: 40px; /* 고정 높이: 20px * 2줄 */
+`;
+
+export default ProductThumbnailInfo;
