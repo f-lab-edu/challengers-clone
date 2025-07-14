d68c60fbadf2ab5cd22481d34287f15a96b672c3
lecto17 <khwoo_rpsit@naver.com>
2025-07-14 12:05:29 +0900

    chore: category setter 함수 대신 타입 구체화된 handler 노출

--- src/hooks/useCategoryState.ts
+++ src/hooks/useCategoryState.ts
@@ -1,7 +1,14 @@
+import { HOME_CATEGORIES } from "@/constants/constants";
 import { useState } from "react";
+
+type HomeCategory = (typeof HOME_CATEGORIES)[number]["enName"];
 
 export default function useCategoryState() {
   const [activeCategory, setActiveCategory] = useState("all");
 
-  return { activeCategory, setActiveCategory };
+  const handleClickCategory = (category: HomeCategory) => {
+    setActiveCategory(category);
+  };
+
+  return { activeCategory, handleClickCategory };
 }
