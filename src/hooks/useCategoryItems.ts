573b251252c3a42f36da36501257c865bc12a796
lecto17 <khwoo_rpsit@naver.com>
2025-07-14 19:16:23 +0900

    chore: viewport 노출 시 data fetching 하는 기능만 남기도록 수정

--- src/hooks/useCategoryItems.ts
+++ src/hooks/useCategoryItems.ts
@@ -1,89 +1,57 @@
 import { PaginatedResponse } from "@/hooks/useInfiniteData";
 import { fetchHomeCategoryItems } from "@/remotes/home";
 import type { HOME_CATEGORY_ITEM } from "@/type/home";
-import { useEffect, useRef, useState } from "react";
 import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
 
 type UseCategoryItemsProps = {
-  isCategoryChanged: boolean;
   activeCategory: string;
   skipFetchWithInitialData?: PaginatedResponse<HOME_CATEGORY_ITEM[]>;
   staleTime?: number;
+  initialPageParam?: number;
+  enabled?: boolean;
 };
 
 export default function useCategoryItems({
-  isCategoryChanged,
   activeCategory,
   skipFetchWithInitialData,
   staleTime = 1000 * 60,
+  initialPageParam = 0,
+  enabled = true,
 }: UseCategoryItemsProps) {
-  const enabledRef = useRef(false);
-  const initialPageParam = isCategoryChanged
-    ? 0
-    : skipFetchWithInitialData?.nextOffset || 0;
+  const { data, isLoading, hasNextPage, fetchNextPage } =
+    useSuspenseInfiniteQuery<PaginatedResponse<HOME_CATEGORY_ITEM[]>>({
+      queryKey: ["/api/home/categories?category", activeCategory],
+      queryFn: async ({ pageParam = 0 }) => {
+        if (!enabled) {
+          return Promise.resolve({
+            data: [],
+            hasNextPage: skipFetchWithInitialData?.hasNextPage || false,
+            nextOffset: skipFetchWithInitialData?.nextOffset || 0,
+          });
+        }
 
-  const [categoryItems, setCategoryItems] = useState<HOME_CATEGORY_ITEM[]>(
-    skipFetchWithInitialData?.data || []
-  );
-
-  const {
-    data: items,
-    isLoading,
-    hasNextPage,
-    fetchNextPage,
-  } = useSuspenseInfiniteQuery<PaginatedResponse<HOME_CATEGORY_ITEM[]>>({
-    queryKey: ["/api/home/categories?category", activeCategory],
-    queryFn: async ({ pageParam = 0 }) => {
-      if (!enabledRef.current) {
-        return Promise.resolve({
-          data: [],
-          hasNextPage: skipFetchWithInitialData?.hasNextPage || false,
-          nextOffset: skipFetchWithInitialData?.nextOffset || 0,
+        const result = await fetchHomeCategoryItems({
+          category: activeCategory,
+          pageParam: pageParam as number,
         });
-      }
 
-      const result = await fetchHomeCategoryItems({
-        category: activeCategory,
-        pageParam: pageParam as number,
-      });
-
-      return result.data;
-    },
-    initialPageParam,
-    getNextPageParam: (lastPage) => {
-      if (lastPage == undefined) return undefined;
-      return lastPage.hasNextPage ? lastPage.nextOffset : undefined;
-    },
-    staleTime,
-  });
-
-  const resetCategoryItems = () => {
-    enabledRef.current = true;
-    setCategoryItems([]);
-  };
-
-  useEffect(() => {
-    if (isCategoryChanged) {
-      resetCategoryItems();
-    }
-  }, [isCategoryChanged]);
-
-  useEffect(() => {
-    const updatedItems = [
-      ...categoryItems,
-      ...(items?.pages[items.pages.length - 1].data ||
-        (isCategoryChanged ? [] : skipFetchWithInitialData?.data || [])),
-    ];
-
-    setCategoryItems(updatedItems);
-  }, [items, skipFetchWithInitialData?.data, activeCategory]);
+        return result.data;
+      },
+      initialPageParam,
+      getNextPageParam: (lastPage) => {
+        if (lastPage == undefined) return undefined;
+        return lastPage.hasNextPage ? lastPage.nextOffset : undefined;
+      },
+      staleTime,
+    });
 
   return {
-    activeCategory,
-    categoryItems,
+    data:
+      data && data.pages.length > 0
+        ? data.pages.flatMap((page) => page.data)
+        : [],
     isLoading,
     hasNextPage,
     fetchNextPage,
-    resetCategoryItems,
   };
 }
