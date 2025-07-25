087d5ec5dfe2f627a1f0ba815c46dae546106927
lecto17 <khwoo_rpsit@naver.com>
2025-07-25 11:38:05 +0900

    feat: input 텍스트 입력 300ms 디바운싱하여 filter 처리

--- src/components/home/HomeAutoComplete.tsx
+++ src/components/home/HomeAutoComplete.tsx
@@ -1,14 +1,21 @@
 import AutoComplete from "../auto-complete/AutoComplete";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { AutoCompleteItem, GroupItem } from "@/type/home";
 import { AUTO_COMPLETE_ITEMS } from "@/constants/constants";
+import useDebounce from "@/hooks/useDebounce";
 
 export default function HomeAutoComplete() {
   const [items, setItems] = useState<AutoCompleteItem[]>([]);
 
-  const handleChangeInput = (value: string) => {
+  const filterItems = useCallback((value: string) => {
     const _filtered = AUTO_COMPLETE_ITEMS.filter(({ name, tags }) => value !== '' && name.startsWith(value) || tags?.some((tag) => tag === value));
     setItems(_filtered);
+  }, []);
+
+  const debouncedFilterItems = useDebounce(filterItems, 300);
+
+  const handleChangeInput = (value: string) => {
+    debouncedFilterItems(value);
   }
 
   return (
