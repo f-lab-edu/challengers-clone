a49e5eda2886e40790335cfc33308b4b54dc80dc
lecto17 <khwoo_rpsit@naver.com>
2025-07-26 13:42:14 +0900

    chore: 액션 아이템 모달 위 모달 출력하여 개인정보 입력받을 수 있도록 작업

--- src/hooks/useActionIconModal.ts
+++ src/hooks/useActionIconModal.ts
@@ -1,6 +1,8 @@
 import SelectModal from "@/components/modal/SelectModal";
 import { useModalStack } from "./useModalStack";
 import { HOME_ACTION_ICON_MODAL_ITEMS } from "@/data/data";
+import ConfirmModal from "@/components/modal/ConfirmModal";
+import InputModal from "@/components/modal/InputModal";
 
 export default function useActionIconModal() {
   const modal = useModalStack();
@@ -12,8 +14,24 @@ export default function useActionIconModal() {
     });
 
     if (selectedItem) {
-      console.log('Selected item:', selectedItem);
+      const confirmed = await modal.open(ConfirmModal, {
+        title: '개인 정보 인증 필요',
+        message: '해당 메뉴에 접근하기 위해 개인정보가 필요합니다.\n입력창으로 이동하시겠습니까?',
+        onCancel: modal.close,
+        onConfirm: () => modal.close(true)
+      })
 
+      if (confirmed === true) {
+        const personalInfo = await modal.open(InputModal, {
+          title: '개인 정보 입력',
+          onCancel: modal.close,
+          onConfirm: (value: string) => modal.close(value)
+        })
+
+        if (personalInfo) {
+
+        }
+      }
     }
 
     return selectedItem;
