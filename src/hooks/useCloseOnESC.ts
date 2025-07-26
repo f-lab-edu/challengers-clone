61e0332bed473d5b1cb4a7d396f444075c8c48b8
lecto17 <khwoo_rpsit@naver.com>
2025-07-26 12:43:27 +0900

    chore: 불필요 props 삭제

--- src/hooks/useCloseOnESC.ts
+++ src/hooks/useCloseOnESC.ts
@@ -1,15 +1,12 @@
 import { useEffect } from "react";
 
 type UseCloseOnESCProps = {
-  enabled: boolean;
   onClose: () => void;
 }
 
-export default function useCloseOnESC({ enabled, onClose }: UseCloseOnESCProps) {
+export default function useCloseOnESC({ onClose }: UseCloseOnESCProps) {
 
   useEffect(() => {
-    if (!enabled) return;
-
     const handleKeyDown = (e: KeyboardEvent) => {
       if (e.key === 'Escape') {
         onClose()
@@ -18,6 +15,6 @@ export default function useCloseOnESC({ enabled, onClose }: UseCloseOnESCProps) 
 
     window.addEventListener('keydown', handleKeyDown);
     return () => { window.removeEventListener('keydown', handleKeyDown) }
-  }, [enabled, onClose])
+  }, [onClose])
   return {}
 }
