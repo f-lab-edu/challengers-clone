71d1e1e1a71452ff14460cb963d1df6d4c4aec2f
lecto17 <khwoo_rpsit@naver.com>
2025-07-27 10:06:03 +0900

    chore: 불필요 props 제거

--- src/components/modal/Dimmed.tsx
+++ src/components/modal/Dimmed.tsx
@@ -4,9 +4,8 @@ import styled, { css } from 'styled-components';
 type DimmedProps = {
   children: React.ReactNode;
   onClose: () => void;
-  forBottomSheet?: boolean;
 }
-export default function Dimmed({ children, onClose, forBottomSheet = false }: DimmedProps) {
+export default function Dimmed({ children, onClose }: DimmedProps) {
   // 다른 PR에서 외부 클릭 시 onClose callback 실행하는 훅 만들어 놓음
   // TODO 추후 해당 훅 적용하여 변경할 것.
   const handleClick: MouseEventHandler<HTMLDivElement> = (e) => {
@@ -17,29 +16,20 @@ export default function Dimmed({ children, onClose, forBottomSheet = false }: Di
 
   return (
     <Wrapper
-      $forBottomSheet={forBottomSheet}
       onClick={handleClick}>
       {children}
     </Wrapper>
   )
 }
 
-const Wrapper = styled.div<{ $forBottomSheet: boolean }>`
+const Wrapper = styled.div`
   width: 100%;
   height: 100%;
   background: rgba(0, 0, 0, 0.5);
   display: flex;
-  
- ${({ $forBottomSheet }) =>
-    $forBottomSheet ?
-      css`
-        flex-direction: column-reverse;
-      `: `
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      `};
-    
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   position: absolute;
   top: 0;
   left: 0;
