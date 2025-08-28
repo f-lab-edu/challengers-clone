87e30e87a2138eba6ccf3f3377d35e7803be0ac3
lecto17 <khwoo_rpsit@naver.com>
2025-08-28 17:10:37 +0900

    chore: survey 홈 화면 생성

---
+++ src/components/survey/SurveyHome.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormProvider, useForm } from "react-hook-form";
+import styled from "styled-components";
+import { questionSchema } from "@/schema/survey-schema";
+import SurveyList from "./SurveyList";
+import Button from "../button/Button";
+
+const SurveyHome = () => {
+  const form = useForm({
+    defaultValues: {
+      questions: [{ title: "", content: "" }],
+    },
+    resolver: zodResolver(questionSchema),
+  });
+  const { handleSubmit } = form;
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <FormProvider {...form}>
+      <Container>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Title>Challegers 이용에 관한 설문을 작성해주세요 🖌</Title>
+          <SurveyList />
+          <Button
+            buttonText="제출하기"
+            buttonType="primary"
+            buttonStyle="fullWidth"
+            onClick={onSubmit}
+          />
+        </Form>
+      </Container>
+    </FormProvider>
+  );
+};
+
+export default SurveyHome;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  padding: 20px;
+`;
+
+const Form = styled.form`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 20px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
