91f863469e0724fb8fde4829f8f746b42645e02e
lecto17 <khwoo_rpsit@naver.com>
2025-08-29 14:28:58 +0900

    chore: RHFInputText 내 name 따라 세부적인 기능 작동되도록 작업

--- src/components/input/RHFInputText.tsx
+++ src/components/input/RHFInputText.tsx
@@ -1,16 +1,98 @@
 "use client";
 
-import { HTMLAttributes } from "react";
-import { useFormContext } from "react-hook-form";
+import { ChangeEvent, InputHTMLAttributes } from "react";
+import {
+  Controller,
+  ControllerRenderProps,
+  FieldValues,
+  useFormContext,
+} from "react-hook-form";
 import styled, { css } from "styled-components";
 import ErrorMessage from "../common/ErrorMessage";
+import SwitchCases from "../common/SwitchCases";
 
-type RHFInputTextProps = HTMLAttributes<HTMLInputElement> & {
+type RHFInputTextProps = InputHTMLAttributes<HTMLInputElement> & {
   name: string;
   label: string;
   required: boolean;
   errorMessage?: string;
 };
+
+// const PlainText = ({
+//   name,
+//   required,
+//   errorMessage,
+//   field,
+//   ...props
+// }: Omit<RHFInputTextProps, "label"> & {
+//   field: ControllerRenderProps<FieldValues, string>;
+// }) => {
+//   return (
+//     <Input
+//       type="text"
+//       id={name}
+//       required={required}
+//       $hasError={!!errorMessage}
+//       {...field}
+//       {...props}
+//     />
+//   );
+// };
+
+// const PhoneText = ({
+//   name,
+//   required,
+//   errorMessage,
+//   field,
+//   ...props
+// }: Omit<RHFInputTextProps, "label"> & {
+//   field: ControllerRenderProps<FieldValues, string>;
+// }) => {
+//   const { setValue } = useFormContext();
+//   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+//     const value = e.target.value;
+//     const formattedValue = value.replace(/[^0-9-]/g, "");
+//     const formattedValueWithDash = formattedValue
+//       .split("")
+//       .reduce((acc, cur, idx) => {
+//         if (idx === 3) {
+//           // 전화번호 3번째 자리인 경우
+//           if (cur === "-") {
+//             return acc + cur;
+//           }
+//           return acc + "-" + cur;
+//         } else if (idx === 8) {
+//           // 전화번호 8번째 자리인 경우
+//           if (cur === "-" || acc[idx - 1] === "-") {
+//             // 대시를 입력하는 경우 혹은 2번째 대시가 이미 있는 경우
+//             return acc + cur;
+//           }
+//           return acc + "-" + cur;
+//         } else {
+//           return acc + cur;
+//         }
+//       }, "");
+//     setValue(name, formattedValueWithDash);
+//   };
+
+//   const handleBlur = () => {
+//     const value = field.value;
+//     setValue(name, value, { shouldValidate: true });
+//   };
+
+//   return (
+//     <Input
+//       type="text"
+//       id={name}
+//       required={required}
+//       $hasError={!!errorMessage}
+//       {...field}
+//       {...props}
+//       onChange={handleChange}
+//       onBlur={handleBlur}
+//     />
+//   );
+// };
 
 export default function RHFInputText({
   name,
@@ -19,26 +101,103 @@ export default function RHFInputText({
   errorMessage,
   ...props
 }: RHFInputTextProps) {
-  const { register } = useFormContext();
+  const { control } = useFormContext();
+
+  const getProps = (field: ControllerRenderProps<FieldValues, string>) => {
+    const { setValue } = useFormContext();
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value;
+      const formattedValue = value.replace(/[^0-9-]/g, "");
+      const formattedValueWithDash = formattedValue
+        .split("")
+        .reduce((acc, cur, idx) => {
+          if (idx === 3) {
+            // 전화번호 3번째 자리인 경우
+            if (cur === "-") {
+              return acc + cur;
+            }
+            return acc + "-" + cur;
+          } else if (idx === 8) {
+            // 전화번호 8번째 자리인 경우
+            if (cur === "-" || acc[idx - 1] === "-") {
+              // 대시를 입력하는 경우 혹은 2번째 대시가 이미 있는 경우
+              return acc + cur;
+            }
+            return acc + "-" + cur;
+          } else {
+            return acc + cur;
+          }
+        }, "");
+      setValue(name, formattedValueWithDash);
+    };
+
+    const handleBlur = () => {
+      const value = field.value;
+      setValue(name, value, { shouldValidate: true });
+    };
+
+    switch (name) {
+      case "phone":
+        return {
+          onChange: handleChange,
+          onBlur: handleBlur,
+        };
+      default:
+        return {};
+    }
+  };
 
   return (
-    <InputWrapper>
-      <Div>
-        <Label htmlFor={name}>{label}</Label>
-        {required && <Star>*</Star>}
-      </Div>
-      <Div>
-        <Input
-          type="text"
-          required={required}
-          id={name}
-          {...register(name)}
-          {...props}
-          $hasError={!!errorMessage}
-        />
-        {errorMessage && <ErrorMessage errorMessage={errorMessage} />}
-      </Div>
-    </InputWrapper>
+    <Controller
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <InputWrapper>
+          <Div>
+            <Label htmlFor={name}>{label}</Label>
+            {required && <Star>*</Star>}
+          </Div>
+          <Div>
+            <Input
+              type="text"
+              id={name}
+              required={required}
+              $hasError={!!errorMessage}
+              {...field}
+              {...props}
+              {...getProps(field)}
+            />
+
+            {/* <SwitchCases
+              value={name}
+              cases={{
+                email: (
+                  <PlainText
+                    name={name}
+                    required={required}
+                    errorMessage={errorMessage}
+                    field={field}
+                    {...props}
+                  />
+                ),
+                phone: (
+                  <PhoneText
+                    name={name}
+                    required={required}
+                    errorMessage={errorMessage}
+                    field={field}
+                    {...props}
+                  />
+                ),
+              }}
+            /> */}
+
+            {errorMessage && <ErrorMessage errorMessage={errorMessage} />}
+          </Div>
+        </InputWrapper>
+      )}
+    />
   );
 }
 
