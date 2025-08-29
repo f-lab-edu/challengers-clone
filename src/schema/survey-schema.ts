1a08227e1ecd88eb77288cda9863c2bb8c54bf78
lecto17 <khwoo_rpsit@naver.com>
2025-08-29 14:29:48 +0900

    chore: zod 전화번호 regex 추가

--- src/schema/survey-schema.ts
+++ src/schema/survey-schema.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 
+const phoneRegex = new RegExp(/^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/);
+
 export const questionSchema = z.object({
   // questions: z.record(z.string(), z.string()),
   email: z.email({ error: "이메일 형식이 올바르지 않습니다." }),
-  phone: z.string(),
+  phone: z.string().regex(phoneRegex, "전화번호 형식이 올바르지 않습니다."),
   gender: z.string(),
   birthday: z.string(),
   recommend: z.string(),
