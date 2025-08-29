36d7911bfedce010d2cd1a77ef82a94015f583b1
lecto17 <khwoo_rpsit@naver.com>
2025-08-29 10:07:37 +0900

    chore: zod email 스키마 추가

--- src/schema/survey-schema.ts
+++ src/schema/survey-schema.ts
@@ -2,7 +2,8 @@ import { z } from "zod";
 
 export const questionSchema = z.object({
   // questions: z.record(z.string(), z.string()),
-  userId: z.string(),
+  email: z.email({ error: "이메일 형식이 올바르지 않습니다." }),
+  phone: z.string(),
   gender: z.string(),
   birthday: z.string(),
   recommend: z.string(),
