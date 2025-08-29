51b423b5a7c44d3c2a265ea304bc82ad0e59e752
lecto17 <khwoo_rpsit@naver.com>
2025-08-29 10:07:23 +0900

    design: error 상태 대응하는 css 작업

--- src/components/input/RHFInputText.tsx
+++ src/components/input/RHFInputText.tsx
@@ -2,35 +2,42 @@
 
 import { HTMLAttributes } from "react";
 import { useFormContext } from "react-hook-form";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import ErrorMessage from "../common/ErrorMessage";
 
 type RHFInputTextProps = HTMLAttributes<HTMLInputElement> & {
   name: string;
   label: string;
   required: boolean;
+  errorMessage?: string;
 };
 
 export default function RHFInputText({
   name,
   label,
   required,
+  errorMessage,
   ...props
 }: RHFInputTextProps) {
-  const {
-    register,
-    formState: { errors },
-  } = useFormContext();
+  const { register } = useFormContext();
 
   return (
     <InputWrapper>
-      <Label htmlFor={name}>{label}</Label>
-      <Input
-        type="text"
-        required={required}
-        id={name}
-        {...register(name)}
-        {...props}
-      />
+      <Div>
+        <Label htmlFor={name}>{label}</Label>
+        {required && <Star>*</Star>}
+      </Div>
+      <Div>
+        <Input
+          type="text"
+          required={required}
+          id={name}
+          {...register(name)}
+          {...props}
+          $hasError={!!errorMessage}
+        />
+        {errorMessage && <ErrorMessage errorMessage={errorMessage} />}
+      </Div>
     </InputWrapper>
   );
 }
@@ -41,7 +48,9 @@ const InputWrapper = styled.div`
   gap: 8px;
 `;
 
-const Input = styled.input`
+const Input = styled.input<{ $hasError: boolean }>`
+  width: 100%;
+  height: 32px;
   border: none;
   border-bottom: 1px solid #bbb;
   padding: 8px 12px;
@@ -50,15 +59,39 @@ const Input = styled.input`
   font-size: 14px;
 
   &:focus {
-    border-bottom: 2px solid #6488ea;
+    ${({ $hasError }) =>
+      $hasError == null &&
+      css`
+        border-bottom: 2px solid #6488ea;
+      `}
     background-color: #eee;
     border-radius: 8px;
     border-bottom-left-radius: 0;
     border-bottom-right-radius: 0;
   }
+
+  ${({ $hasError }) =>
+    $hasError &&
+    css`
+      height: 64px;
+      padding-bottom: 0;
+      border-bottom: 2px solid red;
+    `}
 `;
 
 const Label = styled.label`
   font-size: 18px;
   font-weight: 500;
+`;
+
+const Div = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 8px;
+  position: relative;
+`;
+
+const Star = styled.span`
+  color: red;
+  margin-top: 4px;
 `;
